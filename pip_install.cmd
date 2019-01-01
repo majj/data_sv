1126188d1287f73f32d984369655bafcf8b27914
majj <aidear@163.com>
2019-01-01 22:22:22 +0800

    pip install

--- pip_install.cmd
+++ pip_install.cmd
@@ -1,14 +1,4 @@
 
-pip install -U ujson
-
-pip install -U nameko
-
-pip install -U cx_Oracle
-
-pip install -U redis
-
-rem pip install pendulum
-
-pip install -U DBUtils
+pip install -r requirements.txt
 
 pause
